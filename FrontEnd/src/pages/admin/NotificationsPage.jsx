a5d0df69c7a99d0be62505918b9b90b36f73bd8e
khantalhaahmad <khantalhaahmad978@gmail.com>
2026-09-11 01:00:57 +0530

    updated

--- FrontEnd/src/pages/admin/NotificationsPage.jsx
+++ FrontEnd/src/pages/admin/NotificationsPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NotificationPage = () => {
   return (
     <div style={{ padding: "30px" }}>
-      <h1>Users Page</h1>
+      <h1>Notification's Page</h1>
     </div>
   );
 };
